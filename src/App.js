97abb2b767ecd0f4b14eb65bb9feb54bf363b813
Swapnil Nyayade <swapnilnyayade040@gmail.com>
2023-03-14 09:36:55 +0530

    useReducer Hook

--- src/App.js
+++ src/App.js
@@ -1,75 +1,38 @@
+import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import AddIcon from '@mui/icons-material/Add';
-import DeleteIcon from '@mui/icons-material/Delete';
+import { useReducer } from 'react';
+import Header from './Components/Header';
 
+const reducer = (state, action) =>{
+  console.log("state: ", state)
+  console.log("action: ", action)
+  switch (action.type) {
+    case "INC":
+      return state + 1;
+    
+    case "DEC":
+      return state - 1;
+    case "MUL":
+      return state * 2;
+    case "RES":
+        return state * 0;
+    default:
+      return state;
+  }
+}
 
 function App() {
 
-  // const [name, setName] = useState("")
-  // const [email, setEmail] = useState("")
-  const [form, setForm] = useState({})
-  const [data, setData] = useState([])
-
-  const addData = () => {
-    // setData([...data, {name, email}])
-    // setName("")
-    // setEmail("")
-    setData([...data, form])
-    setForm({ name: "", email: "" })
-
-  }
-
-  const removeItem = (index) => {
-    let arr = data
-    arr.splice(index, 1)
-    setData([...arr])
-  }
+  const [state, dispatch] = useReducer(reducer, 0)
 
   return (
     <div className="App">
-      <Header />
-
-      {/* form */}
-      <div className='form'>
-        <Stack direction="row" spacing={2}>
-          {/* <TextField value={name} onChange={(event)=> setName(event.target.value)} id="outlined-basic" label="name" variant="outlined" />
-        <TextField value={email} onChange={(event)=> setEmail(event.target.value)} id="outlined-basic" label="email" variant="outlined" /> */}
-          <TextField value={form.name} onChange={(event) => setForm({ ...form, name: event.target.value })} id="outlined-basic" label="name" variant="outlined" />
-          <TextField value={form.email} onChange={(event) => setForm({ ...form, email: event.target.value })} id="outlined-basic" label="email" variant="outlined" />
-
-          <Button onClick={addData} variant="contained" color="success"><AddIcon /></Button>
-        </Stack>
-      </div>
-
-      {/* data */}
-      <div className='data'>
-        <div className='data_val'>
-
-          <h4>Name</h4>
-          <h4>Email</h4>
-          <h4>Remove</h4>
-        </div>
-        {
-          data.map((element, index) => {
-            return (
-              <div className='data_val'>
-
-                <h4>{element.name}</h4>
-                <h4>{element.email}</h4>
-
-                <Button onClick={() => removeItem(index)} variant="contained" color="error">
-                  <DeleteIcon />
-                </Button>
-              </div>
-            )
-          })
-        }
-      </div>
+      <Header/>
+      <h1>{state}</h1>
+      <button onClick={()=>dispatch({type: "INC"})}>Increment</button>
+      <button onClick={()=>dispatch({type: "DEC"})}>Decrement</button>
+      <button onClick={()=>dispatch({type: "MUL"})}>Multiply</button>
+      <button onClick={()=>dispatch({type: "RES"})}>Reset</button>
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='header'>
-      <h1>Record Management</h1>
+        <h1>useReducer</h1>
     </div>
   )
 }
